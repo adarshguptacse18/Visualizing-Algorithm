75bcde390f949fcd982e0342051b8376f68ef62e
Adarsh Gupta <adarshgupta.cse18@iitbhu.ac.in>
2020-12-06 15:27:21 +0530

    Convex Hull with Random Points

--- Convex Hull/sketch.js
+++ Convex Hull/sketch.js
@@ -3,10 +3,15 @@ var p;
 var q;
 var hull = [];
 var cur;
+var ind = 0;
+var slider;
 function setup() {
+
   createCanvas(1000, 1000)
   var button = createButton("Start Convex Hull");
-  for (var i = 0; i < 100; i++) {
+  createDiv("Speed")
+  slider = createSlider(1,30,10,1);
+  for (var i = 0; i < 10; i++) {
     points.push(new Point(random(100, height - 100), random(100, width - 100)));
   }
   button.mousePressed(() => {
@@ -19,8 +24,10 @@ function setup() {
     });
     start = points[0];
     p = 0;
+    q = 1;
     start.selected = true;
-    hull = [];
+    hull = [start];
+    ind = 0;
     draw();
   });
 }
@@ -35,29 +42,53 @@ function draw() {
     vertex(p.x, p.y);
   }
   endShape(CLOSE);
-  
-  if (p==undefined) {
+
+  if (p == undefined) {
     noLoop();
     return;
   }
-  if(hull.length>0 && p == 0){
-    p = null;
+  // drawLine(points[q],points[q],color(255));
+  
+
+
+  if (ind === points.length) {
+    if(q == 0){
+      console.log("Done");
+      noLoop();
+      p = null;
+      return;
+    }
+    p = q;
+
+    points[p].selected = true;
+
+    hull.push(points[p]);
+    q = (p + 1) % points.length;
+    ind = 0;
+    loop();
     return;
   }
-  hull.push(points[p]);
-  q = (p + 1) % points.length;
-  points[p].selected = true;
-  for (var i = 0; i < points.length; i++) {
-    const a = p5.Vector.sub(points[int(q)].pos, points[int(p)].pos);
-    const b = p5.Vector.sub(points[i].pos, points[p].pos);
-    const cross = a.cross(b);
-    if (cross.z<0)
-      q = i;
-  }
-  p = q;
-  frameRate(2);
+
+  stroke(0, 255, 0);
+  strokeWeight(2);
+  line(points[p].x, points[p].y, points[q].x, points[q].y);
+
+  let checking = points[ind];
+  stroke(255);
+  line(points[p].x, points[p].y, checking.x, checking.y);
+
+  // for (var i = 0; i < points.length; i++) 
+  var i = ind;
+  const a = p5.Vector.sub(points[int(q)].pos, points[int(p)].pos);
+  const b = p5.Vector.sub(points[i].pos, points[p].pos);
+  const cross = a.cross(b);
+  if (cross.z < 0)
+    q = i;
+  ind += 1;
+  // p = q;
+  frameRate(slider.value());
   loop();
-  
+
 }
 
 
